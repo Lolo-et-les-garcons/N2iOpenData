a8c08cae3597e1b84944afb6d0cff2c4799527cf
AdrianGuery <45165546+AdrianGuery@users.noreply.github.com>
2018-12-06 23:37:16 +0100

    Adrian
    Ajout du parseur csv

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
-
+error_reporting(E_ERROR);
+ini_set('display_errors', E_ERROR);
 /**
  * author: Alexandre Fauchard, Adrian Guerry et Gaëtan Jagorel
  * Date: 2018-12-06
  * Time: 19:06
  */
 
-
+require_once "./Parser.php";
+//get_risque_ville("Lanester");
+get_risque_ville_type("Montpellier", "Séisme");
 
 
 ?>
