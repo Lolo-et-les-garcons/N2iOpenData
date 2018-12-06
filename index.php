3230e8656fc9f92d106416bfdd853cfeb1abf2a2
Ice2o <alexandre.fauchard41@gmail.com>
2018-12-06 19:17:48 +0100

    Ajout de index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * author: Alexandre Fauchard, Adrian Guerry et Gaëtan Jagorel et encore Adrian
+ * author: Alexandre Fauchard, Adrian Guerry et Gaëtan Jagorel
  * Date: 2018-12-06
  * Time: 19:06
  */
